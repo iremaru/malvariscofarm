635d65ecf99e097f3c5e38ba919779717d1edbca
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-05-07 23:25:55 +0100

    add image url pattern

--- malva-risco/next.config.ts
+++ malva-risco/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
 	sassOptions: {
 		includePaths: ["./src/styles"],
 	},
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "eleoxppxrjrvnqdwrdiz.supabase.co",
+				pathname: "/storage/**",
+			},
+		],
+	}
 };
 
 export default nextConfig;
