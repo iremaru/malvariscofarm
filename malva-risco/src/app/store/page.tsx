e2f38ea365c9b399dddd15a8fb30bdbd44aaa6ae
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-05-07 18:52:01 +0100

    update product card style

--- malva-risco/src/app/store/page.tsx
+++ malva-risco/src/app/store/page.tsx
@@ -7,16 +7,19 @@ export default function page() {
 	const fakeData = [
 		{
 			ProductName: "Tomate",
+			ProductDescription: "Rojo pasión, no es nada vergonzón",
 			ProductPrice: "2'5€/kg",
 			ProductImage: "/products/tomate.webp"
 		},
 		{
 			ProductName: "Col",
+			ProductDescription: "No saca sus cuernos al sol, pero mola un montón",
 			ProductPrice: "0'90€/ud",
 			ProductImage: "/products/col.webp"
 		},
 		{
 			ProductName: "Berenjena",
+			ProductDescription: "Una verdura super poderosa y ultra deliciosa",
 			ProductPrice: "0'53€/ud",
 			ProductImage: "/products/berenjena.webp"
 		}
@@ -30,6 +33,7 @@ export default function page() {
 					<ProductCard
 						key={`product_card_${index}`}
 						productName={product.ProductName}
+						productDescription={product.ProductDescription}
 						productPrice={product.ProductPrice}
 						productImage={product.ProductImage}
 					/>
--- malva-risco/src/components/store/productCard/productCard.tsx
+++ malva-risco/src/components/store/productCard/productCard.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+'use client'
+import React, { ChangeEvent, useState } from 'react';
 import Image from 'next/image';
 import style from './productCard.module.scss';
 
 interface ProductModel {
 	productName: string,
+	productDescription: string,
 	productPrice: string,
 	productImage: string;
 }
 
 export const ProductCard = ({
 	productName,
+	productDescription,
 	productPrice,
 	productImage
 }: ProductModel
 ) => {
+	const [productCount, setProductCount] = useState(1)
+	const handleProductCountChange = (changeEvent: ChangeEvent) => {
+		setProductCount((Number)((changeEvent.target as HTMLInputElement).value))
+	}
+
+	const handleAddItemToCart = () => {
+		const item = {
+			item: productName,
+			count: productCount,
+		}
+		console.log(item);
+	}
 
 	return (
 		<div className={style.productCard}>
@@ -23,8 +38,29 @@ export const ProductCard = ({
 				className={style.productImage}
 				width={300}
 				height={300} />
-			<h3 className={style.productName}>{productName}</h3>
-			<p className={style.productPrice}>{productPrice}</p>
+			<div>
+
+				<div className={style.productInfo}>
+					<h3 className={style.productName}>{productName}</h3>
+					<p>{productDescription}</p>
+					<p className={style.productPrice}>{productPrice}</p>
+
+				</div>
+				<div className={style.productActions}>
+					<div className={style.stepper}>
+						<p className={style.button} onClick={handleAddItemToCart}>-</p>
+						<input
+							type='number'
+							min={1}
+							max={9999}
+							defaultValue={1}
+							onChange={handleProductCountChange}
+						/>
+						<p className={style.button} onClick={handleAddItemToCart}>+</p>
+					</div>
+					<button onClick={handleAddItemToCart}>Añadir</button>
+				</div>
+			</div>
 		</div>
 	);
 };
