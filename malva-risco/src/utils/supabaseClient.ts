dcbe11dec2ae6efb7fbb54ba228830764efb5d19
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-05-08 21:00:16 +0100

    refactor supabase client

--- malva-risco/src/utils/supabaseClient.ts
+++ malva-risco/src/utils/supabaseClient.ts
@@ -1,9 +1,2 @@
 import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs';
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createPagesBrowserClient({
-	supabaseUrl,
-	supabaseKey: supabaseAnonKey
-});
+export const supabase = createPagesBrowserClient();
