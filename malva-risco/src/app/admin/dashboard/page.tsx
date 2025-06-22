7ca831b5e3779c8a4db4298de111610dd9e883da
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-06-22 21:47:55 +0100

    add admin dashboard

---
+++ malva-risco/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./dashboard.module.scss";
+
+export default function AdminDashboard() {
+  return <div className={styles.page}>AdminDashboard</div>;
+}
