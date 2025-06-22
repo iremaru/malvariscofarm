ed9ba266f737b2582001a253f75e0d901bac0b9f
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-06-22 21:48:38 +0100

    update not-found page

--- malva-risco/src/app/not-found.tsx
+++ malva-risco/src/app/not-found.tsx
@@ -1,18 +1,11 @@
 "use client";
 import { redirect, usePathname } from "next/navigation";
-import Link from "next/link";
 
 export default function NotFound() {
   const pathname = usePathname();
 
-  if (pathname.startsWith("/admin") && !pathname.includes("/login")) {
-    return (
-      <div>
-        <h2>Not Found</h2>
-        <p>No se puede encontrar la página</p>
-        <Link href='/admin'>Return to admin page</Link>
-      </div>
-    );
+  if (pathname.startsWith("/admin")) {
+    return redirect("/admin/dashboard");
   }
 
   redirect("/home");
