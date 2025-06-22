531918a3b9a9fc6fb0d13a1d46327bca02478bd7
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-06-22 18:01:29 +0100

    add img url

--- malva-risco/src/components/store/productCard/productCard.tsx
+++ malva-risco/src/components/store/productCard/productCard.tsx
@@ -1,61 +1,68 @@
-'use client'
-import React, { ChangeEvent, useState, FC } from 'react';
-import Image from 'next/image';
-import style from './productCard.module.scss';
-import { Batch } from '@/model/store/batch.type';
-
+"use client";
+import React, { ChangeEvent, useState, FC } from "react";
+import Image from "next/image";
+import style from "./productCard.module.scss";
+import { Batch } from "@/model/store/batch.type";
 
 interface ProductCardProps {
-	batch: Batch;
+  batch: Batch;
 }
 
-export const ProductCard: FC<ProductCardProps> = ({ batch }
-) => {
-	const { product, prices } = batch;
-	const { pvp, currency, unit } = prices?.[0];
-	const [productCount, setProductCount] = useState(1)
-	const handleProductCountChange = (changeEvent: ChangeEvent) => {
-		setProductCount((Number)((changeEvent.target as HTMLInputElement).value))
-	}
+export const ProductCard: FC<ProductCardProps> = ({ batch }) => {
+  const { product, prices } = batch;
+  const { pvp, currency, unit } = prices?.[0];
+  const [productCount, setProductCount] = useState(1);
+  const handleProductCountChange = (changeEvent: ChangeEvent) => {
+    setProductCount(Number((changeEvent.target as HTMLInputElement).value));
+  };
 
-	const handleAddItemToCart = () => {
-		const item = {
-			item: product,
-			count: productCount,
-		}
-		console.log(item);
-	}
+  const handleAddItemToCart = () => {
+    const item = {
+      item: product,
+      count: productCount,
+    };
+    console.log(item);
+  };
 
-	return (
-		<div className={style.productCard}>
-			<Image
-				src={product.img}
-				alt={product.sale_name}
-				className={style.productImage}
-				width={300}
-				height={300} />
-			<div>
+  console.log("IMG: ", process.env.NEXT_PUBLIC_IMG_BASE_URL, product.img);
 
-				<div className={style.productInfo}>
-					<h3 className={style.productName}>{product.sale_name}</h3>
-					<p>{product.description}</p>
-					<p className={style.productPrice}>{pvp}{currency}/{unit.shortName}</p>
-				</div>
-				<div className={style.productActions}>
-					<div className={style.stepper}>
-						<p className={style.button} onClick={handleAddItemToCart}>-</p>
-						<input
-							type='number'
-							min={1}
-							max={9999}
-							defaultValue={1}
-							onChange={handleProductCountChange}
-						/>
-						<p className={style.button} onClick={handleAddItemToCart}>+</p>
-					</div>
-					<button onClick={handleAddItemToCart}>Añadir</button>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={style.productCard}>
+      <Image
+        src={`${process.env.NEXT_PUBLIC_IMG_BASE_URL}${product.img}`}
+        alt={product.sale_name}
+        className={style.productImage}
+        width={300}
+        height={300}
+      />
+      <div>
+        <div className={style.productInfo}>
+          <h3 className={style.productName}>{product.sale_name}</h3>
+          <p>{product.description}</p>
+          <p className={style.productPrice}>
+            {pvp}
+            {currency}/{unit.shortName}
+          </p>
+        </div>
+        <div className={style.productActions}>
+          <div className={style.stepper}>
+            <p className={style.button} onClick={handleAddItemToCart}>
+              -
+            </p>
+            <input
+              type='number'
+              min={1}
+              max={9999}
+              defaultValue={1}
+              onChange={handleProductCountChange}
+            />
+            <p className={style.button} onClick={handleAddItemToCart}>
+              +
+            </p>
+          </div>
+          <button onClick={handleAddItemToCart}>Añadir</button>
+        </div>
+      </div>
+    </div>
+  );
 };
