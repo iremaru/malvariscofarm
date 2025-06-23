9d2ad488214eacaae845ffbdc9c532feeb727943
Iremaru <irene_marrero_ruiz@hotmail.com>
2025-06-23 12:17:33 +0100

    update admin shop page

--- malva-risco/src/app/admin/shop/page.tsx
+++ malva-risco/src/app/admin/shop/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Shop() {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1>Administración de la tienda</h1>
+      <div>
+        <h2>Pedidos</h2>
+      </div>
+      <div>
+        <h2>Productos a la venta</h2>
+      </div>
+      <div>
+        <h2>Precios</h2>
+      </div>
+    </div>
+  );
 }
